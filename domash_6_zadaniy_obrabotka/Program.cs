3c2059a9f3424d835bd98da34d96989403d2330b
Zeerck <doroschkewich11@gmail.com>
2020-10-01 17:13:39 +0500

    Добавьте файлы проекта.

---
+++ domash_6_zadaniy_obrabotka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace domash_6_zadaniy_obrabotka
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
